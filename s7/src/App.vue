a3d2a6a3c8a7a71563051d5d5086b1df0a677fc7
Morteza QorbanAlizade <gho.morteza@gmail.com>
2023-01-02 21:42:32 +0330

    session 7 - use components - basic

---
+++ s7/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import UsersList from './components/UsersList.vue';
+
+	
+	export default {
+	components: { UsersList },
+
+		data() {
+			return {
+				users: ["Morteza", "Ali", "Fatemh", "Mojatab", "Farzad"],
+				customers: ["Abbas", "javad", "Naqi", "Hasan", "Morteza", "Ali", "Fatemh", "Mojatab", "Farzad"],
+			};
+		},
+	}
+</script>
+
+<template>
+	<users-list :items="users"></users-list>
+	<hr />
+	<users-list :items="customers"></users-list>
+</template>
---
+++ s7/src/components/UsersList.vue
@@ -0,0 +1,22 @@
+<template>
+	<ul>
+		<li v-for="(user, index) in items" :key="index">
+			{{ index + 1 + "=>" + user }}
+		</li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		props: ["items"],
+		data() {
+			return {};
+		},
+	};
+</script>
+
+<style>
+	ul li {
+		list-style-type: none;
+	}
+</style>
